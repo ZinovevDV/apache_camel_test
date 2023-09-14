e2e205e2b1dd033b92068c0e3af1ac0d26b12788
Zinovev Dmitrii <Zinovev-DV@mail.ru>
2023-09-14 17:01:13 +0300

    empty project

---
+++ src/main/java/ru/mail/zinovev_dv/apache_camel_test/ApacheCamelTestApplication.java
@@ -0,0 +1,13 @@
+package ru.mail.zinovev_dv.apache_camel_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamelTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamelTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mail/zinovev_dv/apache_camel_test/ApacheCamelTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mail.zinovev_dv.apache_camel_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCamelTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
